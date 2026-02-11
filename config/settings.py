d33583903dd83c228feffe54653d4a27a5e41963
chandhlikith7-cpu <chandhlikith7@gmail.com>
2026-02-11 14:42:06 -0600

    Enable production logging

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {
+        "django.request": {"handlers": ["console"], "level": "ERROR", "propagate": False},
+    },
+}
