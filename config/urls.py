44bbef8f3e761f710ef42708a36340cefaf44e3d
chandhlikith7-cpu <chandhlikith7@gmail.com>
2026-02-11 22:54:35 -0600

    Add Cloudinary for images

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("accounts/", include("accounts.urls")),
-
     path("", include("core.urls")),
     path("", include("catalog.urls")),
     path("", include("orders.urls")),
 ]
-
-# ✅ Serve MEDIA in production too (needed on Render for images)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
